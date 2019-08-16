e5c8a4520c620323f1972e096f68d6ed61e2a41c
thewsomeguy <thewsomeguy@gmail.com>
2019-08-15 21:42:48 -0500

    Export TAction_type

--- src/methods/AuditLog.ts
+++ src/methods/AuditLog.ts
@@ -4,7 +4,7 @@ import RequestHandler from "../RequestHandler";
 import { TAuditLogObject } from "../LibTypes";
 
 
-type TAction_type = 1 | 10 | 11 | 12 | 13| 14 | 15| 20 | 21 | 22 | 23| 24 | 25| 30 | 31| 32 | 40 | 41 | 42 | 50 | 51 | 52 | 60 | 61 | 62 | 72
+export type TAction_type = 1 | 10 | 11 | 12 | 13| 14 | 15| 20 | 21 | 22 | 23| 24 | 25| 30 | 31| 32 | 40 | 41 | 42 | 50 | 51 | 52 | 60 | 61 | 62 | 72
 
 interface Tdata {
     user_id: string,
@@ -12,7 +12,7 @@ interface Tdata {
     before: string,
    /***
     * @default 50
-    */ 
+    */
    limit: number
 }
 
--- types/src/methods/AuditLog.d.ts
+++ types/src/methods/AuditLog.d.ts
@@ -1,6 +1,6 @@
 import RequestHandler from "../RequestHandler";
 import { TAuditLogObject } from "../LibTypes";
-declare type TAction_type = 1 | 10 | 11 | 12 | 13 | 14 | 15 | 20 | 21 | 22 | 23 | 24 | 25 | 30 | 31 | 32 | 40 | 41 | 42 | 50 | 51 | 52 | 60 | 61 | 62 | 72;
+export declare type TAction_type = 1 | 10 | 11 | 12 | 13 | 14 | 15 | 20 | 21 | 22 | 23 | 24 | 25 | 30 | 31 | 32 | 40 | 41 | 42 | 50 | 51 | 52 | 60 | 61 | 62 | 72;
 interface Tdata {
     user_id: string;
     action_type: TAction_type;
